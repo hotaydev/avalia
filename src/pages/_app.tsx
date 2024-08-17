2bf4e26e27a9fca78f4eaf2bd2dd28763c5c6b41
TaylorHo <taylor@hotay.dev>
2024-08-17 14:24:46 -0300

    chore: added compatibility with older web engines

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary";
 import type { AppProps } from "next/app";
 
+// Used these two dependencies to have compatibility with older browsers (like IOS 12 and IOS 10)
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
